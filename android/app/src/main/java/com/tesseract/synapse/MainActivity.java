b2271ea1fd5e8475b4149b0335d8d3ac6c294fb4
Aleksa Zdravković <aleksazdravkovic@proton.me>
2024-06-16 12:05:18 +0200

    Cleaned up and set up the project

---
+++ android/app/src/main/java/com/tesseract/synapse/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tesseract.synapse;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography'
+import daisyui from 'daisyui'
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [typography, daisyui]
+}
